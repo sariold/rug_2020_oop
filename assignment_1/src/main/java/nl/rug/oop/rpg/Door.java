aafa41f3c185795ffcd2834c35fdf09e5b94a032
Diego Sariol <sarioldiego@yahoo.com>
2020-04-24 13:32:47 +0200

    setting up door arraylist

---
+++ assignment_1/src/main/java/nl/rug/oop/rpg/Door.java
@@ -0,0 +1,11 @@
+package nl.rug.oop.rpg;
+
+public class Door extends DungeonObjects {
+
+    public Door(String description) {
+        super(description);
+    }
+
+
+
+}
--- assignment_1/src/main/java/nl/rug/oop/rpg/Room.java
+++ assignment_1/src/main/java/nl/rug/oop/rpg/Room.java
@@ -1,8 +1,22 @@
 package nl.rug.oop.rpg;
 
-public class Room extends DungeonObjects{
+import java.util.ArrayList;
+
+public class Room extends DungeonObjects {
+
+    private ArrayList<Door> doors;
 
     public Room(String description) {
         super(description);
+        this.doors = new ArrayList<Door>();
     }
+
+    public void addDoor(Door door) {
+        doors.add(door);
+    }
+
+    public ArrayList<Door> getDoors() {
+        return this.doors;
+    }
+
 }
