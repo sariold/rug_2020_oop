2d00ebe9f727564ef1c9201087ed2cb6a6334063
Diego Sariol <sarioldiego@yahoo.com>
2020-04-24 13:06:40 +0200

    working doors, set up abstract class for rooms and doors, and set up inspectable interface

---
+++ assignment_1/src/main/java/nl/rug/oop/rpg/Doors.java
@@ -0,0 +1,9 @@
+package nl.rug.oop.rpg;
+
+public class Doors extends DungeonObjects {
+
+    public Doors(String description) {
+        super(description);
+    }
+
+}
---
+++ assignment_1/src/main/java/nl/rug/oop/rpg/DungeonObjects.java
@@ -0,0 +1,14 @@
+package nl.rug.oop.rpg;
+
+public abstract class DungeonObjects implements Inspectable{
+    private String description;
+
+    public DungeonObjects(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public void inspect() {
+        System.out.println(description);
+    }
+}
