26bd82a44662d3e73c24fc4acde79602767ff8ea
pasman9710 <felixzailskas@gmail.com>
2020-04-28 21:42:18 +0200

    Dragon is now a boss and can use fire/ice magic

---
+++ assignment_1/src/main/java/nl/rug/oop/rpg/Boss.java
@@ -0,0 +1,8 @@
+package nl.rug.oop.rpg;
+
+public class Boss extends Enemy{
+
+    public Boss(String description, String name, int hitPoints, int attackPoints, int goldValue){
+        super(description, name, hitPoints, attackPoints, goldValue);
+    }
+}
--- assignment_1/src/main/java/nl/rug/oop/rpg/Dragon.java
+++ assignment_1/src/main/java/nl/rug/oop/rpg/Dragon.java
@@ -1,6 +1,8 @@
 package nl.rug.oop.rpg;
 
-public class Dragon extends Enemy {
+import java.util.Random;
+
+public class Dragon extends Boss {
 
     public Dragon(String description, String name) {
         super(description, name, DefaultStats.DRAGON_HIT_POINTS , DefaultStats.DRAGON_ATTACK_POINTS, DefaultStats.DRAGON_GOLD_VALUE);
@@ -8,6 +10,22 @@ public class Dragon extends Enemy {
 
     public Dragon(String name) {
         this("Do not test me child for I am a mighty Dragon! Engage me and perish in flames!", name);
+    }
+
+    @Override
+    public void attack(Attackable attacked) {
+        Random r = new Random();
+        int chance = r.nextInt(101);
+        if (chance < 21) {
+            ((Player) attacked).burn();
+        } else {
+            chance = r.nextInt(101);
+            if (chance < 21) {
+                ((Player) attacked).freeze();
+            } else {
+                super.attack(attacked);
+            }
+        }
     }
 
     @Override
